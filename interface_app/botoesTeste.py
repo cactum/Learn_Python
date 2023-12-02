49a9cd4c03075eb032e6abf590416e4ece8ecc1c
Cactum <andi.arrudaalves@gmail.com>
2023-12-02 12:21:43 -0300

    alterações de botoes

--- interface_app/botoesTeste.py
+++ interface_app/botoesTeste.py
@@ -1,18 +1,20 @@
 """from kivy.app import App
 from kivy.uix.button import Button
 
-class ButtonApp(App):
+class MainApp(App):
     def build(self):
-        return Button(text='Fala Galera!',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5},
-                      on_press=self.on_press_button)
+        button = Button(text='Teste Botão ação',
+                        size_hint=(.5, .5),
+                        pos_hint={'center_x': .5, 'center_y': .5})
+        button.bind(on_press=self.on_press_button)
+
+        return button
 
     def on_press_button(self, instance):
         print('Você apertou o botão!')
 
 if __name__ == '__main__':
-    app = ButtonApp()
+    app = MainApp()
     app.run()
 """
 
